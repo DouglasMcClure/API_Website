6a494410c82afc524fd9b5962ec5dbb0edd9b1dd
dougl <douglasmc115@gmail.com>
2022-04-13 04:12:02 -0400

    Added Each HTML File and Included Functions for each API

--- app.py
+++ app.py
@@ -13,9 +13,38 @@ def get_db_connection():
     return conn
 
 
+# Call/Collect data from all database tables
 @app.route('/')
-def messario_api():
+def messario_news():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM messario_news').fetchall()
     conn.close()
     return render_template('news.html', posts=posts)
+
+
+def cryptocompare_coin_info():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM cryptocompare_coin_info').fetchall()
+    conn.close()
+    return render_template('coin_info.html', posts=posts)
+
+
+def finnhub_candles():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM finnhub_candles').fetchall()
+    conn.close()
+    return render_template('candles.html', posts=posts)
+
+
+def coinlore_coin_market():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM coinlore_coin_market').fetchall()
+    conn.close()
+    return render_template('coin_market.html', posts=posts)
+
+
+def trade_assets():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM trade_assets').fetchall()
+    conn.close()
+    return render_template('assets.html', posts=posts)
