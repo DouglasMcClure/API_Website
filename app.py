ac62408f013da9b91ef15226e6e39ae0824cd615
dougl <douglasmc115@gmail.com>
2022-05-01 18:47:33 -0400

    Candles and other CSS

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlite3
 import chart_studio.tools as tls
 import plotly
 from dash import Dash, dcc, html, Input, Output
+from flask import Markup
 from flask import Flask, g, render_template
 from flask_navigation import Navigation
 import pandas as pd
@@ -20,6 +21,7 @@ nav.Bar('top', [
     nav.Item('Trade Assets', 'trade_assets'),
     nav.Item('Coin Info', 'cryptocompare_coin_info'),
 ])
+
 
 # app.layout = html.Div([
 #     html.H4('Apple stock candlestick chart'),
@@ -80,8 +82,6 @@ def finnhub_candles():
                                          close=close_price)])
     # dcc.Graph(figure=fig)
     py.write_html(fig, file='plotly.html', auto_open=True)
-    print(py.write_html(fig, file='plotly.html', auto_open=True))
-    tls.get_embed('file:///C:/Users/dougl/PycharmProjects/API_Website1/plotly.html')  # change to your url
     conn.close()
     return render_template('candles.html')
 
